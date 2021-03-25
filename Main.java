76f62bbe9a3cc8fc1078dd025728f1d063f3c6a4
ChukwuEmeka1269 <ukwuomaemeka546@yahoo.com>
2021-03-25 14:02:00 +0100

    This program computes sum, product, differences and quotient of two integers.

--- Main.java
+++ Main.java
@@ -1,70 +1,52 @@
 package com.emekafirstjavaprogram;
 
 
-import java.text.NumberFormat;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        //This program accepts 2 integers from users
+        //Computes the sum, product, difference and quotient
 
-        //Call the readNumber Class to read in values
-        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
-        float annualInterest = (float) readNumber("Annual Interest: ", 1, 30);
-        byte period = (byte) readNumber("Period(year): ", 1, 30);
+        int number1;//declare first number
+        int number2; //declared second number
+        int sum;
+        int product;
+        int difference;
+        int quotient;
 
-        //Calculate the Mortgage by calling the calculateMortgage method
-        double mortgage = calculateMortgage(principal, annualInterest, period);
+        Scanner input = new Scanner(System.in);//Calling the Scanner class
+        System.out.print("First NUmber: ");//prompt user
+        number1 = input.nextInt();//read in the first value
 
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Your monthly mortgage is: " + mortgageFormatted);
+        System.out.print("Second Number: ");//prompt user
+        number2 = input.nextInt(); //read in the second value
 
+        //Start computation of sum
+        System.out.printf("Sum is: %d", sum = number1 + number2);
 
-    }
+        //compute product
+        System.out.printf("\nProduct is: %d ", product = number1 * number2);
 
-    public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
-        double value;
+        //compute difference
+        System.out.printf("\nDifference is: %d ", difference = (number2 - number1));
 
-        while(true){
-            System.out.print(prompt);
-            value = scanner.nextDouble();
-            if(value >= min && value <= max)
-                break;
-            System.out.println("Enter a number between " + min + " and" + " " + max);
-        }
-
-        return value;
-    }
+        //compute quotient
+        System.out.printf("\nQuotient is: %d", quotient = number1 % number2);
 
 
-    public static double calculateMortgage(
-            int principal,
-            float annualInterest,
-            byte period) {
 
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
 
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        short numOfPay = (short)(period * MONTHS_IN_YEAR);
 
-        return principal *
-                ((monthlyInterest * (Math.pow(1 + monthlyInterest, numOfPay))
-                        /(Math.pow(1 + monthlyInterest, numOfPay) - 1)));
+
+
+
 
     }
 
 }
-
-/* Summary of this program:
-The program calculates the monthly mortgage payment for a principal loan collected at an annual interest for some period of years.
-Two methods created:
-1. readNumber method created to accepted valid inputs
-2. calculateMortgage method to calculate the monthly mortgage payment.
-
-The two methods are called in to the main class to execute the valid inputs provided
- */
 
 
 
